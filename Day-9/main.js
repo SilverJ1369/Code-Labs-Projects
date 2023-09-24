b1c3aa6e827bbbbbc7b0d96b7d8a5a24e0d8e776
SilverJ1369 <35865016+SilverJ1369@users.noreply.github.com>
2023-09-24 11:38:47 -0500

    finished with required features

--- Day-9/main.js
+++ Day-9/main.js
@@ -9,6 +9,8 @@ input.addEventListener('focusout', endFocus);
 form.addEventListener('submit', createTask);
 taskList.addEventListener('submit', console.log('submitted'))
 
+// const allTasks = [];
+
 function focusInput() {
 	body.classList.add('focus-form');
 }
@@ -24,17 +26,62 @@ function createTask(e) {
     const taskContainer = document.createElement('li');
     const newTask = document.createElement('p');
     const newTaskCheck = document.createElement('input');
+    const removeButton = document.createElement('button');
 
     newTaskCheck.setAttribute('type', 'checkbox');
-    newTaskCheck.className = 'check';
+    newTaskCheck.addEventListener('click', crossoutP);
 
     newTask.className = 'task';
     newTask.innerText = task;
     newTask.target = '_blank';
 
+    removeButton.className = 'btn-remove';
+    removeButton.innerText = 'Remove';
+    removeButton.addEventListener('click', removeTask);
+    
+
     taskContainer.appendChild(newTaskCheck);
     taskContainer.appendChild(newTask);
+    taskContainer.appendChild(removeButton);
     taskList.appendChild(taskContainer);
+
+    // allTasks.push(task);
+
+    // populateTaskList(allTasks);
+
+    // function populateTaskList(tasks = []) {
+    //     taskList.innerHTML = tasks
+    //         .map(  
+    //             (link =>
+    //                 `<li> <input type="check"> <p class="task" target="_blank">${task}</p> <button class="btn-remove>Remove</button>`)
+    //             )
+    //         .join("");
+    // }
+
+    function crossoutP() {
+        if(this.checked) {
+            newTask.classList.add('crossout-p');
+        } else {
+            if (newTask.classList.contains('crossout-p'))  {
+                newTask.classList.remove('crossout-p');
+            }
+        
+          }
+    }
+
+    function removeTask() {
+        console.log('remove');
+        taskContainer.remove();
+    }
+
     endFocus();
     form.reset();
+}
+
+function testcheck() {
+    if(this.checked) {
+        console.log('checked');
+    } else {
+        console.log('not checked');
+    }
 }
